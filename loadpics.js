313a705b6670b1b96f9d2b0b460c407b01549a13
Andrew <unsafe.andrew@shitposting.expert>
2023-04-22 19:55:03 +0200

    Fix the pics loading

--- loadpics.js
+++ loadpics.js
@@ -2,7 +2,7 @@ window.onload = ev => {
 	const pics_container = document.getElementById("pics-container");
 	const random_pics = Array.from({length: 9}, () => Math.floor(Math.random() * 442))
 		// Map them to the image url
-		.map(num => `<img src="/images/${num}.png">`)
+		.map(num => `<img src="/cv/images/${num}.png">`)
 		// Join them into a string
 		.join("");
 	pics_container.innerHTML = random_pics;
